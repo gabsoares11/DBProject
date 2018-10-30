8cdecc5ddb335bca1a6194c22bc327b3093a814a
yallydlgaldino <yally.galdino@ccc.ufcg.edu.br>
2018-10-30 12:53:05 -0300

    exclusao

---
+++ exclusao.sql
@@ -0,0 +1,21 @@
+drop TABLE Aluno;
+
+drop TABLE Professor;
+
+drop TABLE Departamento;
+
+drop TABLE TelefoneDepartamento;
+
+drop TABLE LinhaPesquisa;
+
+drop TABLE Laboratorio;
+
+drop TABLE Recurso;
+
+drop TABLE Projeto;
+
+drop TABLE AgenciaFinanciadora;
+
+drop TABLE Publicacao;
+
+drop TABLE Patente;	
