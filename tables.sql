06ea4123951992241c91a3601dd6e53a94a44f63
larissaamorim <34272746+larissaamorim@users.noreply.github.com>
2018-11-01 19:45:28 -0300

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -175,3 +175,23 @@ CREATE TABLE AlunoParticipa (
 
 
 
+
+DROP TABLE Professor;
+DROP TABLE Aluno;
+DROP TABLE Departamento;
+DROP TABLE LinhaPesquisa;
+DROP TABLE Laboratorio;
+DROP TABLE Recurso;
+DROP TABLE Projeto;
+DROP TABLE AgenciaFinanciadora;
+DROP TABLE Publicacao;
+DROP TABLE Patente;	
+
+DROP TABLE TelefoneAgencia;
+DROP TABLE TelefoneDepartamento;
+DROP TABLE financia;
+DROP TABLE executa;
+DROP TABLE linhaPesquisa_contem;
+DROP TABLE esta_em;
+DROP TABLE ProfessorParticipa;
+DROP TABLE AlunoParticipa;
